63cdb2304822cbbd121f0b647c574efacc35a0b5
developer <developer@domain.com>
2019-03-31 09:48:02 +0900

    ナビゲーションバーを整形

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/About">About</router-link>
-      <router-link to="/Skill">Skill</router-link>
-      <router-link to="/Product">Product</router-link>
-      <router-link to="/Social">Social</router-link>
-      <router-view/>
+    <nav class="menu-bar">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/About">About</router-link></li>
+        <li><router-link to="/Skill">Skill</router-link>
+        <li><router-link to="/Product">Product</router-link></li>
+        <li><router-link to="/Social">Social</router-link></li>
+      </ul>
     </nav>
+    <router-view/>
   </div>
 </template>
 
@@ -26,5 +27,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu-bar {
+  text-align: center;
+}
+
+.menu-bar ul {
+  margin: 0 ;
+  padding: 0 ;
+}
+
+.menu-bar li {
+  list-style: none;
+  display: inline-block;
+  width: 10%;
+  min-width: 90px;
+}
+
+.menu-bar li:not(:last-child) {
+  border-right:2px solid #ddd;
+}
+
+.menu-bar a {
+  text-decoration: none;
+  color: #333;
+}
+
+.menu-bar a.current {
+  color: #00B0F0;
+  border-bottom:2px solid #00B0F0;
+}
+.menu-bar a:hover {
+  color:#F7CB4D;
+  border-bottom:2px solid #F7CB4D;
 }
 </style>
