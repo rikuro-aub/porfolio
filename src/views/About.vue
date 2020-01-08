bf2dd0aa94cc7ed77c47d4c660327ece881f43aa
3no3 <ulis7524@gmail.com>
2020-01-08 23:12:08 +0900

    自己紹介文を修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
     <div class="about">
         <h1>About</h1>
         <img src="@/assets/about_img.png">
-        <p>弱小SIerに勤務しておりました。</p>
-        <p>Web系に転職したくて奮闘中。</p>
+        <p>SESで5年ほど開発を経験してきました。</p>
+        <p>Web系サービスの開発に挑戦したくて奮闘中。</p>
         <p>フルスタックエンジニアになりたい!!</p>
     </div>
 </template>
